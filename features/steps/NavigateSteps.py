e8044eea23e8ef2da515964dc49e5cc0f2ae6d99
Sergio <sergguz@gmail.com>
2022-06-20 22:16:12 +0000

    Initial commit

---
+++ features/steps/NavigateSteps.py
@@ -0,0 +1,54 @@
+import time
+
+from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome("driver/chromedriver.exe")
+driver.get("https://flaskapptfm.azurewebsites.net/")
+
+@given('I am at the home page')
+def step_homepage(self):
+    driver.maximize_window()
+    time.sleep(10)
+    element = ""
+    try:
+        element = driver.find_element(By.XPATH, "/html/body/div[2]/form/h3").text
+    except:
+        Exception("Homepage. Element not found")
+
+    if element == "Ingresar":
+        print("Step Home page. Value is equal to 'Ingresar', test passed")
+        assert True
+    else:
+        print("Step Home page. Value is NOT equal to 'Ingresar', test failed")
+        assert False
+
+@when('I press the Register button')
+def step_registerbutton(self):
+    try:
+        driver.find_element(By.ID, "signUp").click()
+        print("Step Register Button, test passed")
+        assert True
+    except:
+        print("Step Register Button, test failed")
+        Exception("Register button. Element not found")
+        assert False
+
+
+@Then('I should see the register form')
+def step_registerform(self):
+    element = ""
+    try:
+        element = driver.find_element(By.XPATH, "/html/body/div/form/h3").text
+    except:
+        Exception("Register form. Element not found")
+
+    if element == "Registrarse":
+        print("Step Register Form. Value is equal to 'Registrarse', test passed")
+        assert True
+    else:
+        print("Step Register Form. Value is NOT equal to 'Registrarse', test failed")
+        assert False
+    time.sleep(10)
+    driver.close()
---
+++ main.py
@@ -0,0 +1,7 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+
